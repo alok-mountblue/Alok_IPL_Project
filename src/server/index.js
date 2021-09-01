1ef486b714bbcc5c1c6e10b2223567f49f259c14
alok <alok.sharma.17@mountblue.tech>
2021-09-01 10:40:05 +0530

    update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -3,20 +3,20 @@ const { matchesPlayedPerYear, matchesWonByEachTeam, extraRunsByEachTeam, topEcon
 const fs = require('fs');
 const csv = require('csvtojson');
 
-const MATCHES_FILE_PATH = '../data/matches.csv';
-const DELIVERIES_FILE_PATH = '../data/deliveries.csv';
+const matchesPath = '../data/matches.csv';
+const DeliveriesPath= '../data/deliveries.csv';
 
-const JSON_OUTPUT_FILE_PATH_FIRST = '../public/output/matchesPerYear.json';
-const JSON_OUTPUT_FILE_PATH_SECOND = '../public/output/matchesWonEachTeam.json';
-const JSON_OUTPUT_FILE_PATH_THIRD = '../public/output/extraRunsConcededByEachTeam.json';
-const JSON_OUTPUT_FILE_PATH_FOURTH = '../public/output/economicalBowlers.json';
+const JsonOutputFilePathFirst = '../public/output/matchesPerYear.json';
+const JsonOutputFilePathSecond = '../public/output/matchesWonEachTeam.json';
+const JsonOutputFilePathThird = '../public/output/extraRunsConcededByEachTeam.json';
+const JsonOutputFilePathFourth = '../public/output/economicalBowlers.json';
 
 function main() {
         csv()
-                .fromFile(MATCHES_FILE_PATH)
+                .fromFile(matchesPath)
                 .then((matches) => {
                         csv()
-                                .fromFile(DELIVERIES_FILE_PATH)
+                                .fromFile(DeliveriesPath)
                                 .then((deliveries) => {
                                         const matchesPlayed = matchesPlayedPerYear(matches);
                                         const matcheWonPerYear = matchesWonByEachTeam(matches);
@@ -31,19 +31,19 @@ main();
 
 function savePlayersData(matchesPlayed, matcheWonPerYear,extraRunConceded, resultEcoBowl) {
         
-        fs.writeFile(JSON_OUTPUT_FILE_PATH_FIRST, JSON.stringify(matchesPlayed), 'utf8', (err) => { 
+        fs.writeFile(JsonOutputFilePathFirst, JSON.stringify(matchesPlayed), 'utf8', (err) => { 
                 if (err) {
                         console.log(err);
                 }});
-        fs.writeFile(JSON_OUTPUT_FILE_PATH_SECOND, JSON.stringify(matcheWonPerYear), 'utf8', (err) => {
+        fs.writeFile(JsonOutputFilePathSecond, JSON.stringify(matcheWonPerYear), 'utf8', (err) => {
                 if (err) {
                         console.log(err);
                 }});
-        fs.writeFile(JSON_OUTPUT_FILE_PATH_THIRD, JSON.stringify(extraRunConceded), 'utf8', (err) => { 
+        fs.writeFile(JsonOutputFilePathThird, JSON.stringify(extraRunConceded), 'utf8', (err) => { 
                 if (err) {
                         console.log(err);
                 }});
-        fs.writeFile(JSON_OUTPUT_FILE_PATH_FOURTH, JSON.stringify(resultEcoBowl), 'utf8', (err) => { 
+        fs.writeFile(JsonOutputFilePathFourth, JSON.stringify(resultEcoBowl), 'utf8', (err) => { 
                 if (err) {
                           console.log(err);
                 }});

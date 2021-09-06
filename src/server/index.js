781f6d37feec6f7d7e2d13b57a0e2ad75852047e
alok <alok.sharma.17@mountblue.tech>
2021-09-06 08:48:09 +0530

    update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -1,51 +1,45 @@
-const { matchesPlayedPerYear, matchesWonByEachTeam, extraRunsByEachTeam, topEconomicalBowlers } = require('./ipl');
 
 const fs = require('fs');
 const csv = require('csvtojson');
 
-const matchesPath = '../data/matches.csv';
-const deliveriesPath= '../data/deliveries.csv';
+const MATCHES_FILE_PATH = '../data/matche.csv';
+const DELIVERIES_FILE_PATH = '../data/deliveries.csv';
 
-const JsonOutputFilePathFirst = '../public/output/matchesPerYear.json';
-const JsonOutputFilePathSecond = '../public/output/matchesWonEachTeam.json';
-const JsonOutputFilePathThird = '../public/output/extraRunsConcededByEachTeam.json';
-const JsonOutputFilePathFourth = '../public/output/economicalBowlers.json';
+const JSON_OUTPUT_FILE_PATH_FIRST = '../public/output/matchesPerYear.json';
+const JSON_OUTPUT_FILE_PATH_SECOND = '../public/output/matchesWonEachTeam.json';
+const JSON_OUTPUT_FILE_PATH_THIRD = '../public/output/extraRunsConcededByEachTeam.json';
+const JSON_OUTPUT_FILE_PATH_FOURTH = '../public/output/economicalBowlers.json';
+
+const { matchesPlayedPerYear, matchesWonByEachTeam, extraRunsByEachTeam, topEconomicalBowlers } = require('./ipl');
 
 function main() {
         csv()
-                .fromFile(matchesPath)
+                .fromFile(MATCHES_FILE_PATH)
                 .then((matches) => {
                         csv()
-                                .fromFile(deliveriesPath)
+                                .fromFile(DELIVERIES_FILE_PATH)
                                 .then((deliveries) => {
                                         const matchesPlayed = matchesPlayedPerYear(matches);
                                         const matcheWonPerYear = matchesWonByEachTeam(matches);
                                         const extraRunConceded = extraRunsByEachTeam(matches, deliveries);
                                         const resultEcoBowl = topEconomicalBowlers(matches, deliveries);
 
-                                        savePlayersData(matchesPlayed, matcheWonPerYear, extraRunConceded, resultEcoBowl);
-                                });
-                });
+                                         savePlayerData(matchesPlayed, JSON_OUTPUT_FILE_PATH_FIRST)
+                                         savePlayerData(matcheWonPerYear, JSON_OUTPUT_FILE_PATH_SECOND)
+                                         savePlayerData(extraRunConceded, JSON_OUTPUT_FILE_PATH_THIRD)
+                                         savePlayerData(resultEcoBowl, JSON_OUTPUT_FILE_PATH_FOURTH);
+                                })
+                                .catch(e => console.error(e.message)); 
+                })
+                .catch(e => console.error(e.message));
 }
 main();
 
-function savePlayersData(matchesPlayed, matcheWonPerYear,extraRunConceded, resultEcoBowl) {
+function savePlayerData(data, location) {
         
-        fs.writeFile(JsonOutputFilePathFirst, JSON.stringify(matchesPlayed), 'utf8', (err) => { 
+        fs.writeFile(location, JSON.stringify(data), 'utf8', (err) => { 
                 if (err) {
                         console.log(err);
-                }});
-        fs.writeFile(JsonOutputFilePathSecond, JSON.stringify(matcheWonPerYear), 'utf8', (err) => {
-                if (err) {
-                        console.log(err);
-                }});
-        fs.writeFile(JsonOutputFilePathThird, JSON.stringify(extraRunConceded), 'utf8', (err) => { 
-                if (err) {
-                        console.log(err);
-                }});
-        fs.writeFile(JsonOutputFilePathFourth, JSON.stringify(resultEcoBowl), 'utf8', (err) => { 
-                if (err) {
-                          console.log(err);
                 }});
 }
 
